c56accf5c61b16f8af1298c4ede2c78c329266f4
joao7zy <plugraro5@gmail.com>
2025-09-03 17:59:42 -0300

    Update and rename olamundo.c to jogodecartas.c

---
+++ jogodecartas.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+int main() {
+    // Atributos das cartas
+    int carta1_forca = 80, carta1_velocidade = 60, carta1_inteligencia = 70;
+    int carta2_forca = 75, carta2_velocidade = 65, carta2_inteligencia = 85;
+
+    int escolha;
+    printf("=== SUPER TRUNFO DIGITAL ===\n");
+    printf("Escolha o atributo para comparar:\n");
+    printf("1 - Forca\n");
+    printf("2 - Velocidade\n");
+    printf("3 - Inteligencia\n");
+    printf("4 - Forca + Velocidade (media)\n");
+    printf("5 - Velocidade + Inteligencia (media)\n");
+    printf("Opcao: ");
+    scanf("%d", &escolha);
+
+    switch (escolha) {
+        case 1: // Comparação simples - if
+            printf("\nComparando FORCA...\n");
+            if (carta1_forca > carta2_forca) {
+                printf("Carta 1 venceu! (%d > %d)\n", carta1_forca, carta2_forca);
+            } else if (carta1_forca < carta2_forca) {
+                printf("Carta 2 venceu! (%d > %d)\n", carta2_forca, carta1_forca);
+            } else {
+                printf("Empate! Ambas tem %d de forca.\n", carta1_forca);
+            }
+            break;
+
+        case 2: // Comparação simples - if-else
+            printf("\nComparando VELOCIDADE...\n");
+            if (carta1_velocidade > carta2_velocidade)
+                printf("Carta 1 venceu! (%d > %d)\n", carta1_velocidade, carta2_velocidade);
+            else if (carta1_velocidade < carta2_velocidade)
+                printf("Carta 2 venceu! (%d > %d)\n", carta2_velocidade, carta1_velocidade);
+            else
+                printf("Empate! Ambas tem %d de velocidade.\n", carta1_velocidade);
+            break;
+
+        case 3: // Comparação simples - if-else if
+            printf("\nComparando INTELIGENCIA...\n");
+            if (carta1_inteligencia > carta2_inteligencia)
+                printf("Carta 1 venceu! (%d > %d)\n", carta1_inteligencia, carta2_inteligencia);
+            else if (carta1_inteligencia < carta2_inteligencia)
+                printf("Carta 2 venceu! (%d > %d)\n", carta2_inteligencia, carta1_inteligencia);
+            else
+                printf("Empate! Ambas tem %d de inteligencia.\n", carta1_inteligencia);
+            break;
+
+        case 4: { // Comparação de dois atributos usando operador ternário
+            printf("\nComparando FORCA + VELOCIDADE (media)...\n");
+            float media1 = (carta1_forca + carta1_velocidade) / 2.0;
+            float media2 = (carta2_forca + carta2_velocidade) / 2.0;
+
+            (media1 > media2) ? printf("Carta 1 venceu! (%.1f > %.1f)\n", media1, media2)
+            : (media1 < media2) ? printf("Carta 2 venceu! (%.1f > %.1f)\n", media2, media1)
+            : printf("Empate! Ambas tem media %.1f.\n", media1);
+            break;
+        }
+
+        case 5: { // Outro exemplo com ternário
+            printf("\nComparando VELOCIDADE + INTELIGENCIA (media)...\n");
+            float media1 = (carta1_velocidade + carta1_inteligencia) / 2.0;
+            float media2 = (carta2_velocidade + carta2_inteligencia) / 2.0;
+
+            (media1 > media2) ? printf("Carta 1 venceu! (%.1f > %.1f)\n", media1, media2)
+            : (media1 < media2) ? printf("Carta 2 venceu! (%.1f > %.1f)\n", media2, media1)
+            : printf("Empate! Ambas tem media %.1f.\n", media1);
+            break;
+        }
+
+        default:
+            printf("Opcao invalida!\n");
+            break;
+    }
+
+    printf("\nFim de jogo!\n");
+    return 0;
+}
+
